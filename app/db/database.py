2338e504af9ef999d496bdba2417ed8cf0994dee
Thiago Silva <creditoexpress.thiagosilva@luizalabs.com>
2026-09-09 13:23:01 -0300

    Estrutura inicial do projeto

---
+++ app/db/database.py
@@ -0,0 +1,14 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from app.core.config import settings
+from app.db.base import Base
+
+
+engine = create_engine(settings.database_url)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine,
+)
